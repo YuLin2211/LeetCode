e863fa137e60192fc6da2baa8d90f4925fd8fa17
YuLin2211 <162358113+YuLin2211@users.noreply.github.com>
2025-05-19 18:10:30 +0100

    File updated at 5/19/2025, 6:10:32 PM

--- 7-ReverseInteger/7-ReverseInteger.java
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -1,40 +1,22 @@
-// Last updated: 5/19/2025, 6:09:59 PM
+// Last updated: 5/19/2025, 6:10:32 PM
 class Solution {
-    public int myAtoi(String s) {
-        if(s == null || s.isEmpty()){
-            return 0;
-        }
+    public int reverse(int x) {
+        int rev = 0;
 
-        int i = 0;
-        int n = s.length();
-        int sign = 1;
-        int result = 0;
+        while(x != 0){
+            int pop = x % 10;
+            x /= 10;
 
-        while(i < n && s.charAt(i) == ' '){
-            i++;
-        }
-
-        if(i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
-            if(s.charAt(i) == '+'){
-                sign = 1;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0; // overflow
             }
-            else if(s.charAt(i) == '-'){
-                sign = -1;
-            }
-            i++;
-        }
-
-        while(i < n && Character.isDigit(s.charAt(i))){
-            int digit = s.charAt(i) - '0';
-
-            if (result > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0; // underflow
             }
 
-            result = result * 10 + digit;
-            i++;
+            rev = rev * 10 + pop;
         }
 
-        return result * sign;
+        return rev;
     }
 }
