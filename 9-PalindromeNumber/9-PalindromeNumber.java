ffbcc887e09a754156493d7d132b85959ff46946
YuLin2211 <162358113+YuLin2211@users.noreply.github.com>
2025-06-01 22:44:50 +0100

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 6/1/2025, 10:44:50 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
