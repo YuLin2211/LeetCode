55dbaa697111dc821c1dc55443e48e0ba302b3a2
YuLin2211 <162358113+YuLin2211@users.noreply.github.com>
2025-06-03 00:02:18 +0100

    File updated at 6/3/2025, 12:02:19 AM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,10 +1,13 @@
-// Last updated: 6/3/2025, 12:01:45 AM
+// Last updated: 6/3/2025, 12:02:19 AM
 class Solution {
     public int search(int[] nums, int target) {
         int index = -1;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == target){
                 index = i;
+                break;
+            }
+            if(nums[i] > target){
                 break;
             }
         }
