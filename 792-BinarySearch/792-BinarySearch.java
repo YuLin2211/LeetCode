ba8a07ad55577f9b0b33be78f19e1db0f5c7ca58
YuLin2211 <162358113+YuLin2211@users.noreply.github.com>
2025-06-03 00:03:43 +0100

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,13 @@
+// Last updated: 6/3/2025, 12:03:43 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int index = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
