8a095cfd49b4f1313496e802cf667253eee7b2c2
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-14 20:53:36 +0100

    File updated at 9/14/2025, 8:53:37 PM

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,13 @@
+// Last updated: 9/14/2025, 8:53:37 PM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
