5711809dd6e2709f444b52e4024132b1f083230d
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-14 21:28:53 +0100

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -0,0 +1,18 @@
+// Last updated: 9/14/2025, 9:28:53 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
