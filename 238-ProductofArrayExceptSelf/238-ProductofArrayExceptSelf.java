c3b6959f233bd3d66b54da7088a5f4d7b452fe6a
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-15 14:53:10 +0100

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -0,0 +1,23 @@
+// Last updated: 9/15/2025, 2:53:12 PM
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int maxCount = 0;
+        int best = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int i = s.charAt(right) - 'A';
+            freq[i]++;
+            maxCount = Math.max(maxCount, freq[i]);
+
+            while (right - left + 1 - maxCount > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            best = Math.max(best, right - left + 1);
+        }
+        return best;
+    }
+}
