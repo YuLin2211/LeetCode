711b25289ff020244f9eaf09b7f3a7f91cd8dd17
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-17 00:41:46 +0100

    File updated at 9/17/2025, 12:41:46 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -1,15 +1,25 @@
-// Last updated: 9/17/2025, 12:28:38 AM
+// Last updated: 9/17/2025, 12:41:46 AM
 class Solution {
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        for(int i = 0; i < flowerbed.length && n > 0; i++){
-            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
-                flowerbed[i] = 1;
-                n--;
-                if (n == 0) {
-                    return true;
-                }
+    public String reverseVowels(String s) {
+        char[] seperatedS = s.toCharArray();
+        int i = 0, j = seperatedS.length - 1;
+
+        while (i < j) {
+            while (i < j && !isVowel(seperatedS[i])) i++;
+            while (i < j && !isVowel(seperatedS[j])) j--;
+            if (i < j) {
+                char tmp = seperatedS[i];
+                seperatedS[i] = seperatedS[j];
+                seperatedS[j] = tmp;
+                i++; j--;
             }
         }
-        return n == 0;
+        return new String(seperatedS);
+    }
+
+    private boolean isVowel(char c) {
+        // covers both lowercase and uppercase
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
