0bf5ca596b11d472ebe09bb1a03207a0ad589141
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-17 01:18:03 +0100

    File updated at 9/17/2025, 1:18:03 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -1,24 +1,12 @@
-// Last updated: 9/17/2025, 12:42:37 AM
+// Last updated: 9/17/2025, 1:18:03 AM
 class Solution {
-    public String reverseVowels(String s) {
-        char[] seperatedS = s.toCharArray();
-        int i = 0, j = seperatedS.length - 1;
-
-        while (i < j) {
-            while (i < j && !isVowel(seperatedS[i])) i++;
-            while (i < j && !isVowel(seperatedS[j])) j--;
-            if (i < j) {
-                char tmp = seperatedS[i];
-                seperatedS[i] = seperatedS[j];
-                seperatedS[j] = tmp;
-                i++; j--;
-            }
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        for(int i = 0; i < words.length / 2; i++){
+            String temp = words[i];
+            words[i] = words[words.length - 1 - i];
+            words[words.length - 1 - i] = temp;
         }
-        return new String(seperatedS);
-    }
-
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+        return String.join(" ", words);
     }
 }
