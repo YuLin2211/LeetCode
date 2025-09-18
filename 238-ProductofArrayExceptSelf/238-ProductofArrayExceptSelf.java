b74e1ea39796db84c3d168d722d59a4330585df6
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-18 03:22:45 +0100

    File updated at 9/18/2025, 3:22:45 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,19 +1,32 @@
-// Last updated: 9/18/2025, 3:09:37 AM
+// Last updated: 9/18/2025, 3:22:45 AM
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-        double max = 0;
-        double sum = 0;
+    public int maxVowels(String s, int k) {
+        char[] c = s.toCharArray();
+        int count = 0;
+        int answer = 0;
 
         for(int i = 0; i < k; i++){
-            sum += nums[i];
-        }
-        max = sum;
-
-        for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];
-            max = Math.max(max, sum);
+            if(isVowel(c[i])){
+                count++;
+            }
         }
 
-        return max/k;
+        answer = Math.max(answer, count);
+        if (answer == k) return answer;
+
+        for(int i = k; i < c.length; i++){
+            if(isVowel(c[i])){
+                count++;
+            }
+            if (isVowel(c[i-k])){
+                count--;
+            }
+            answer = Math.max(answer, count);
+        }
+        return answer;
+    }
+
+    private boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
