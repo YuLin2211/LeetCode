275cd87fcdeab5544d7aa1137c6854ad620a7da8
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-18 02:49:33 +0100

    File updated at 9/18/2025, 2:49:33 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,14 +1,19 @@
-// Last updated: 9/18/2025, 2:39:41 AM
+// Last updated: 9/18/2025, 2:49:33 AM
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int left = 0;
-        for(int right = 0; right < nums.length; right++){
-            if(nums[right] != 0){
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0){
+            return true;
+        }
+        char[] array = t.toCharArray();
+        int current = 0;
+        for(int i = 0; i < array.length; i ++){
+            if (array[i] == s.charAt(current)) {
+                current++;
+                if (current == s.length()) {
+                    return true;
+                }
             }
         }
+        return current == s.length();
     }
 }
