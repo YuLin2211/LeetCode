5df571ac4d323c530adc98eba504286e065872cb
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-18 13:27:56 +0100

    File updated at 9/18/2025, 1:27:58 PM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,6 +1,6 @@
-// Last updated: 9/18/2025, 1:21:13 PM
+// Last updated: 9/18/2025, 1:27:58 PM
 class Solution {
-    public int longestOnes(int[] nums, int k) {
+    public int longestSubarray(int[] nums) {
         int left = 0;
         int zeros = 0; 
         int max = 0;
@@ -8,11 +8,11 @@ class Solution {
         for (int right = 0; right < nums.length; right++) {
             if (nums[right] == 0) zeros++;
 
-            while (zeros > k) {
+            while (zeros > 1) {
                 if (nums[left] == 0) zeros--;
                 left++;
             }
-            max = Math.max(max, right - left + 1);
+            max = Math.max(max, right - left);
         }
         return max;
     }
