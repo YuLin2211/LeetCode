94c07a34ded8fbe73d93478ff93be871590ab90f
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-18 13:06:41 +0100

    Create 1456-MaximumNumberofVowelsinaSubstringofGivenLength

---
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -0,0 +1,32 @@
+// Last updated: 9/18/2025, 1:06:43 PM
+class Solution {
+    public int maxVowels(String s, int k) {
+        char[] c = s.toCharArray();
+        int count = 0;
+        int answer = 0;
+
+        for(int i = 0; i < k; i++){
+            if(isVowel(c[i])){
+                count++;
+            }
+        }
+
+        answer = Math.max(answer, count);
+        if (answer == k) return answer;
+
+        for(int i = k; i < c.length; i++){
+            if(isVowel(c[i])){
+                count++;
+            }
+            if (isVowel(c[i-k])){
+                count--;
+            }
+            answer = Math.max(answer, count);
+        }
+        return answer;
+    }
+
+    private boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
