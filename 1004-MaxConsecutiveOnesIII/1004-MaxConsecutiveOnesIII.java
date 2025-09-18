5bb81d0b4a48c47b50b1204c10dabbbbe80333be
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-18 13:48:56 +0100

    File updated at 9/18/2025, 1:48:58 PM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,14 +1,20 @@
-// Last updated: 9/18/2025, 1:42:39 PM
+// Last updated: 9/18/2025, 1:48:58 PM
 class Solution {
-    public int largestAltitude(int[] gain) {
-        int highestPoint = 0;
-        int current = 0;
-        for(int i = 0; i < gain.length; i++){
-            current += gain[i];
-            if(current > highestPoint){
-                highestPoint = current;
-            }
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        int leftTotal = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
         }
-        return highestPoint;
+        
+        for (int i = 0; i < nums.length; i++) {
+            int rightTotal = total - leftTotal - nums[i];
+            if (rightTotal == leftTotal) {
+                return i;
+            }
+            leftTotal += nums[i];
+        }
+
+        return -1;        
     }
 }
