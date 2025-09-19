e1649ce3aa647b76a80d889bd1e70d3c0da7680b
Jia Hao Yu <162358113+YuLin2211@users.noreply.github.com>
2025-09-19 23:49:25 +0100

    File updated at 9/19/2025, 11:49:27 PM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,14 +1,34 @@
-// Last updated: 9/19/2025, 11:30:36 PM
+// Last updated: 9/19/2025, 11:49:27 PM
 class Solution {
-    public String removeStars(String s) {
-        StringBuilder c = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '*') {
-                c.deleteCharAt(c.length() - 1);
-            } else {
-                c.append(s.charAt(i));
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < asteroids.length; i++){
+            int number = asteroids[i];
+            if(number > 0){
+                list.add(number);
+            }
+            else{
+                boolean alive = true;
+                while (alive && !list.isEmpty() && list.get(list.size() - 1) > 0) {
+                    int top = list.get(list.size() - 1);
+                    if (top < -number) {
+                        list.remove(list.size() - 1);
+                    } else if (top == -number) {
+                        list.remove(list.size() - 1);
+                        alive = false;
+                    } else {
+                        alive = false;
+                    }
+                }
+                if (alive) {
+                    list.add(number);
+                }
             }
         }
-        return c.toString();
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) result[i] = list.get(i);
+        return result;
     }
 }
